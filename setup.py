3e3b315c7edd2e571eb9322778661042572bd374
Hidetaka <u687502j@ecs.osaka-u.ac.jp>
2025-05-08 19:48:29 +0900

    change setup.py

--- setup.py
+++ setup.py
@@ -11,11 +11,10 @@ setup(
     zip_safe=False,
     install_requires=[],
     python_requires=">=3.6",
-    license="Apache License 2.0",
+    license="Apache-2.0",
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3",
     ],
     entry_points={
@@ -25,4 +24,13 @@ setup(
             "samplettn=ttnopt:sample",  # Link ttnopt_sample command to your sample function
         ],
     },
+    install_requires=[
+        "numpy>=1.23.5",
+        "jaxlib<0.4.34",
+        "tensornetwork",
+        "networkx",
+        "pydot",
+        "pyyaml",
+        "dotmap",
+    ],
 )
